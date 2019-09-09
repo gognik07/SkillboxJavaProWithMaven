7ff12a5373095a8500948ad5e555646d221d0716
gognik <gognik1990@gmail.com>
2019-09-09 20:45:46 +0300

    refactoring

--- src/main/java/lesson9/tasks4/Main.java
+++ src/main/java/lesson9/tasks4/Main.java
@@ -2,19 +2,10 @@ package lesson9.tasks4;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
 import java.io.*;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
 
@@ -49,8 +40,6 @@ public class Main {
                 fos.write(response);
                 fos.flush();
                 fos.close();
-            } catch (MalformedURLException ex) {
-                ex.printStackTrace();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
